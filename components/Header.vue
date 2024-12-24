df73af1efe8ececc0e0e11a5416a6549b343598d
Mirna Triani <141211528+mirnatriani@users.noreply.github.com>
2024-12-24 14:53:46 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+    <div class="navbar-brand ms-5 ">
+      <img src="~/assets/img/logo.png" alt="Logo" width="50" class="d-inline-block align-text-top">
+      
+    </div>
+    <div class="col-lg-2">
+
+      SMKN 4 TASIKMALAYA
+    </div>
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button> -->
+    <div class="justify-content-end me-5 collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Berita</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kontak</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Ekstrakurikuler
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="container-fluid p-5">
+      <div>
+        <img src="assets/img/smk.jpg" class="d-block w-100" alt="">
+      </div>
+      <br>
+      <div class="text-center">
+        <h1>Berita Terkini</h1>
+      </div>
+      
+    </div>
+  </div> 
+  
+    <div class="container">
+    <div class="row">
+      <div class="col-lg-4 p-2">
+        <img src="/assets/img/ahm.jpg" alt="" style="width: 18rem; height: 20rem;">
+      </div>
+      <div class="col-lg-4 p-2">
+        <img src="/assets/img/Job.jpg" alt="" style="width: 20rem; height: 20rem;">
+      </div>
+      <div class="col-lg-4 p-2">
+        <img src="/assets/img/zona.jpg" alt="" style="width: 20rem; height: 20rem;">
+      </div>
+    </div>
+  </div>
+  
+  <br>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <p class="card-text">VISI MISI</p>
+  </div>
+</div>
+    </div>
+    <div class="col order-5">
+      <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="col order-1">
+      <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+
+  <br>
+      <div class="text-center">
+        <h1>Kompetensi Keahlian</h1>
+      </div>
+      <div class="col-lg-4 p-2">
+        <img src="/assets/img/tbsm.jpg" alt="" style="width: 20rem; height: 20rem;">
+      </div>
+      
+</template>
+

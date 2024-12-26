d5b9f30ddf10754150b12c4b00a6a2337815cb7b
Mirna Triani <141211528+mirnatriani@users.noreply.github.com>
2024-12-26 13:12:35 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -9,32 +9,59 @@
       </div>
     </div>
 
+    <!--card-->
+    <div class="container">
+      <div class="row my-5 ms-5">
+        <div class="col-lg-4">
+          <div class="card" style="height: 10rem;">
+            <div class="card-body bg-primary">
+              <h5 class="card-title text-md-center mt-5">Visi Misi</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card" style="height: 10rem;">
+            <div class="card-body bg-primary">
+              <h5 class="card-title text-md-center mt-5">Kepala Sekolah</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card" style="height: 10rem;">
+            <div class="card-body bg-primary">
+              <h5 class="card-title text-md-center mt-5">Tentang Sekolah</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!--Berita-->
     <div class="container">
       <h1 class="text-center mb-5">Berita Terkini</h1>
       <div class="row justify-content-center">
         <div class="col-lg-4 d-flex justify-content-center">
           <div class="card" style="width: 18rem">
-            <img src="/assets/img/ahm.jpg" class="card-img-top" alt="..." />
+            <img src="/assets/img/berita2.jpg" class="card-img-top" alt="berita" />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">PT DAM Jalin Kerjasama dengan SMKN 4 Tasikmalaya Terapkan
+                Kurikulum Teknik dan Bisnis Sepeda Motor Honda</h5>
               <p class="card-text">
-                PT DAM Jalin Kerjasama dengan SMKN 4 Tasikmalaya Terapkan
-                Kurikulum Teknik dan Bisnis Sepeda Motor Honda
+                PT Daya Adicipta Motora (DAM) selaku Main Dealer Sepeda Motor dan Suku Cadang Honda di Jawa Barat meresmikan kerjasama dengan SMK Negeri 4  Tasikmalaya yang mengimplementasikan Kurikulum Teknik dan Bisnis Sepeda Motor (KTBSM) Astra Honda sekaligus diangkat sebagai Tempat Uji Kompetensi (TUK) versi Astra Honda Motor.
               </p>
               <a
                 href="https://radartasik.disway.id/read/643944/pt-dam-jalin-kerjasama-dengan-smkn-4-tasikmalaya-terapkan-kurikulum-teknik-dan-bisnis-sepeda-motor-honda"
                 class="btn btn-primary"
-                >Go somewhere</a
+                >Selengkapnya</a
               >
             </div>
           </div>
         </div>
         <div class="col-lg-4 d-flex justify-content-center">
           <div class="card" style="width: 18rem">
-            <img src="/assets/img/safety.jpg" class="card-img-top" alt="..." />
+            <img src="/assets/img/berita3.jpg" class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">DAM Gelar Safety Riding Fomo di SMKN 4 Kota Tasikmalaya</h5>
               <p class="card-text">
                 Kampanyekan Cari_Aman Bersama Jurnalis, DAM Gelar Safety Riding
                 Fomo di SMKN 4 Kota Tasikmalaya Artikel ini telah tayang di
@@ -43,39 +70,82 @@
                 Tasikmalaya,
               </p>
               <a
-                href="https://jabar.tribunnews.com/2024/07/24/kampanyekan-cari_aman-bersama-jurnalis-dam-gelar-safety-riding-fomo-di-smkn-4-kota-tasikmalaya.
-                Penulis: Aldi M Perdana | Editor: Kemal Setia Permana"
+                href="https://jabar.tribunnews.com/2024/07/24/kampanyekan-cari_aman-bersama-jurnalis-dam-gelar-safety-riding-fomo-di-smkn-4-kota-tasikmalaya"
                 class="btn btn-primary"
-                >Go somewhere</a
+                >Selengkapnya</a
               >
             </div>
           </div>
         </div>
         <div class="col-lg-4 d-flex justify-content-center">
           <div class="card" style="width: 18rem">
-            <img src="/assets/img/berita.webp" class="card-img-top" alt="..." />
+            <img src="/assets/img/berita1.webp" class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">SMKN 4 Tasikmalaya Raih Beasiswa dari Yayasan Astra Honda Motor</h5>
               <p class="card-text">
                 Tujuh Siswa SMKN 4 Tasikmalaya Raih Beasiswa dari Yayasan Astra
                 Honda Motor
               </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <a href="https://radartasik.id/2024/10/07/tujuh-siswa-smkn-4-tasikmalaya-raih-beasiswa-dari-yayasan-astra-honda-motor/" class="btn btn-primary">Selengkapnya</a>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-  <div class="text-center">
+  <div class="container">
     <h1 class="text-center mb-5">Kompetensi Keahlian</h1>
-  </div>
-  <div class="col-lg-4 p-2">
-    <img
-      src="/assets/img/teesem.png"
-      alt=""
-      style="width: 20rem; height: 20rem"
-    />
+    <div class="row d-flex justify-content-around">
+      <div class="col-lg-6">
+        <div class="col-lg-4 p-2">
+          <img src="/assets/img/pplg.jpeg" alt=" " style="width: 20rem; height: 20rem;">
+        </div>
+        <div class="col-md-6">
+          <h5 class="card-title text-center text-md-start">
+            Pengembangan Perangkat Lunak Dan Gim(PPLG)
+          </h5>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="col-lg-4 p-2">
+          <img src="/assets/img/toi.jpeg" alt=" " style="width: 20rem; height: 20rem;">
+        </div>
+        <div class="col-md-6">
+          <h5 class="card-title text-center text-md-start">
+            Teknik Otomasi Industri(TOI)
+          </h5>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="col-lg-4 p-2">
+          <img src="/assets/img/tbsm.png" alt=" " style="width: 20rem; height: 20rem;">
+        </div>
+        <div class="col-md-6">
+          <h5 class="card-title text-center text-md-start">
+            Teknis Dan Bisnis Sepeda Motor(TBSM)
+          </h5>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="col-lg-4 p-2">
+          <img src="/assets/img/tjkt.jpeg" alt=" " style="width: 20rem; height: 20rem;">
+        </div>
+        <div class="col-md-6">
+          <h5 class="card-title text-center text-md-start">
+            Teknik Jaringan Komputer Dan Telekomunikasi(TJKT)
+          </h5>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="col-lg-4 p-2">
+          <img src="/assets/img/dkv.jpeg" alt=" " style="width: 20rem; height: 20rem;">
+        </div>
+        <div class="col-md-6">
+          <h5 class="card-title text-center text-md-start">
+            Desain Komunikasi Visual(DKV)
+          </h5>
+        </div>
+      </div>
+    </div>
   </div>
 </template>

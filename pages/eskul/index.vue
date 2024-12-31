1b5c29dd3b80cb4699d2d6574bd3dc5927894a41
Mirna Triani <141211528+mirnatriani@users.noreply.github.com>
2024-12-31 12:47:47 +0700

    Add files via upload

--- pages/eskul/index.vue
+++ pages/eskul/index.vue
@@ -2,51 +2,11 @@
   <div class="container pb-5">
     <h2 class="text-center pt-3">Ekstrakurikuler</h2>
     <div class="row flex justify-content-center">
-      <div class="col-lg-3">
-        <div class="card" style="width: 18rem">
-          <img src="/assets/img/laskar.jpg" class="card-img-top" alt="..." />
+      <div v-for="(eskul, i) in cover" :key="i" class="col-lg-3">
+        <div class="card mb-5" style="width: 18rem">
+          <img :src="eskul.poto" class="card-img-top" alt="..." />
           <div class="card-body">
-            <p class="card-text">Laskar Pustaka</p>
-            <p>
-              Meningkatkan minat baca dan pengelolaan perpustakaan untuk
-              mendukung literasi siswa.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="card" style="width: 18rem">
-          <img src="/assets/img/pmr.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <p class="card-text">Palang Merah Remaja</p>
-            <p>
-              Melatih siswa untuk siap siaga dalam memberikan pertolongan
-              pertama dan kegiatan kemanusiaan.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="card" style="width: 18rem">
-          <img src="/assets/img/pks.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <p class="card-text">Patroli Keamanan Sekolah</p>
-            <p>
-              Menanamkan disiplin dan tanggung jawab untuk menjaga ketertiban di
-              lingkungan sekolah.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="card" style="width: 18rem">
-          <img src="/assets/img/pd.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <p class="card-text">Perisai Diri</p>
-            <p>
-              Ekstrakurikuler seni bela diri tradisional untuk melatih fisik dan
-              mental sekaligus menjaga warisan budaya.
-            </p>
+            <h5 class="card-title">{{ eskul.nama }}</h5>
           </div>
         </div>
       </div>
@@ -60,11 +20,11 @@ const supabase = useSupabaseClient();
 const cover = ref([]);
 
 const getCover = async () => {
-  const { data } = await supabase.from("eskul").select('*');
+  const { data } = await supabase.from("eskul").select("*");
   if (data) cover.value = data;
 };
 
 onMounted(() => {
   getCover();
-})
+});
 </script>
--- pages/galeri/index.vue
+++ pages/galeri/index.vue
@@ -2,12 +2,26 @@
   <div class="container pb-5">
     <h2 class="text-center">Galeri</h2>
     <div class="row">
-      <div class="col-lg-3">
+      <div v-for="(data, i) in galeri" :key="i" class="col-lg-3">
         <div class="card" style="width: 18rem">
-          <img src="/assets/img/ahm.jpg" class="card-img-top" alt="..." />
-          <div class="card-body"></div>
+          <img :src="data.poto" class="card-img-top" alt="..." />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient();
+
+const galeri = ref([]);
+
+const getData = async () => {
+  const { data, error } = await supabase.from("galeri").select(`*`);
+  if (data) galeri.value = data;
+};
+
+onMounted(() => {
+  getData();
+});
+</script>
